cfa919017f88584550eba47fb9d1620f8cdcb173
Günter Zeyer <gz@gz-laptop.(none)>
2012-12-26 13:31:31 +0100

    erste Version Uhr

---
+++ uhr_1.py
@@ -0,0 +1,17 @@
+import pygame, sys, math, datetime
+
+windowMargin	= 30
+windowWidth	= 600
+windowHeight	= windowWidth
+windowCenter	= windowWidth/2, windowWidth/2
+clockMarginWidth= 20
+secondColor	= (255,0,0)
+minuteColor	= (100, 200, 0)
+hourColor	= (100, 200, 0)
+clockMarginColor= (130, 130, 0)
+clockBackgroundColor = (20, 40, 30)
+backgroundColor	= (255, 255, 255)
+hourCursorLength= windowWidth/2.0-windowMargin-140
+minuteCursorLength= windowWidth/2.0-windowMargin-40
+secondCursorLength= windowWidth/2.0-windowMargin-10
+
